1d45ddd813966e66a2a9188b26b3fa036a3c8d8b
HARISHGV5 <harishganeshkumar05@gmail.com>
2021-08-30 21:20:36 +0530

    workstarted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from "./components/header";
+import AboutUs from "./components/aboutus";
+import TricksterCalc from './components/trickstercalc';
 
 function App() {
   return (
     <div>
-      
+      <Header></Header>
+      <TricksterCalc></TricksterCalc>
+      <AboutUs></AboutUs>
     </div>
   );
 }
---
+++ src/components/trickstercalc.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TricksterCalc() {  
+  
+    return(
+      <div> 
+        
+         
+        
+      </div>
+    )   
+  }
+  
+  export default TricksterCalc;
