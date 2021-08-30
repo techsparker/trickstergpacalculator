a073a7d1e9a5eea3cfba94392535d122f373b64b
HARISHGV5 <harishganeshkumar05@gmail.com>
2021-08-30 23:06:13 +0530

    header

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,11 @@ function Header() {
   
     return(
       <div> 
-        
+        <html>
+        <body>
+        <h1 style="text-align:center;"><b>TRICKSTER</b></h1>
+        </body>
+        </html>
          
         
       </div>
