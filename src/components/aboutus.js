376071b86702c5669c99ce2a8d8737d910400da8
Harish <harish.ganeshkumar@artspot.in>
2024-05-18 17:18:52 +0530

    Removed About US and Mail ID

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -11,10 +11,10 @@ function AboutUs() {
       <p className="general"><i><b>GPA means Grade Point Average.<br/> GPA is calculated by dividing the <br/> average of a student by the total <br/> credit hours that have been attended.</b></i><br/></p>
       <h4 className="general2"><b>WHAT IS CGPA?</b></h4>
       <p className="general"><i><b>CGPA means Cumulative Grade Point<br/>Average.It is the sum total of the SGPA's<br/>(Subject Grade Point Average)of all semesters<br/>or that of an academic year. </b></i></p>
-      <h2 className="general"><b>ABOUT US:</b></h2>
+      {/* <h2 className="general"><b>ABOUT US:</b></h2>
       <h4 className="general2"><b>TRICKSTER GPA CALCULATOR<br/>TAMIL-NADU.</b></h4>
       <h2 className="general"><i>IF ANY ENQUIRES,PLEASE MAIL US:</i></h2>
-      <p className="general2"><i>1.<u>harishganeshkumar05@gmail.com<br/></u></i><i>2.<u>ashwinhiro01@gmail.com</u></i></p>
+      <p className="general2"><i>1.<u>harishganeshkumar05@gmail.com<br/></u></i><i>2.<u>ashwinhiro01@gmail.com</u></i></p> */}
       
       
          
